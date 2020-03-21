36c50ab6261778842603b6f1d66d94b6f4c80277
jagdeepbaidwan <60941776+jagdeepbaidwan@users.noreply.github.com>
2020-03-21 11:49:38 -0400

    Delete External.h
    Merged by mistake.

--- employee_payroll/External.h
+++
@@ -1,8 +0,0 @@
-int valid_phone(char[]);
-int valid_email(char []);
-int isleap(int );
-int datevalid(int , int , int );
-int notempty(char [] );
-int emp_management(int ,int );
-void emp_detail_mgmt(int);
-void emp_display_details(int);
