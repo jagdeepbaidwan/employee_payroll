aca7a4613b77ce23259448a6d3c7032234f4ecfe
jagdeepbaidwan <60941776+jagdeepbaidwan@users.noreply.github.com>
2020-03-21 10:14:16 -0400

    Add files via upload

--- employee_payroll/External.h
+++ employee_payroll/External.h
@@ -4,6 +4,4 @@ int isleap(int );
 int datevalid(int , int , int );
 int notempty(char [] );
 int emp_management(int ,int );
-void employee(int);
 void emp_detail_mgmt(int);
-void emp_attendance_mgmt(int);
