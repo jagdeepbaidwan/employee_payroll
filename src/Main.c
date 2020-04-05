52157ac51f007be0221cb9cb214ac227be37ff70
varinderdhanda <varinderdhanda@cmail.carleton.ca>
2020-04-04 21:49:59 -0400

    Strucutre created

---
+++ src/Main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+#include <stdlib.h>
+#include<windows.h>
+#include<mysql.h>
+#include<string.h>
+#include "External.h"
+
+
+int main(int argc, char *argv[]) {
+
+return 0;
+}
