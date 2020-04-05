15a90bf701bb49c2aca8e213b5571e06579e694f
varinderdhanda <varinderdhanda@cmail.carleton.ca>
2020-04-04 23:46:50 -0400

    Updated project strucutre

---
+++ include/External.h
@@ -0,0 +1,9 @@
+int valid_phone(char[]);
+int valid_email(char []);
+int isleap(int );
+int datevalid(int , int , int );
+int notempty(char [] );
+int emp_management(int ,int );
+void emp_detail_mgmt(char +);
+void emp_display_details(int);
+void change_password(int );
