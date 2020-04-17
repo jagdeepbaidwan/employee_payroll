150493cb8c665165c49f337b32c0a1c9546a2bee
jagdeepbaidwan <baidwanjagdeep94@gmail.com>
2020-04-17 17:35:48 -0400

    header files commented

--- include/salary_management.h
+++ include/salary_management.h
@@ -1,2 +1,29 @@
+/** @file salary_management.h
+ */
+#ifndef salary_management_H
+#define salary_management_H
+
+/**  @brief Function prototypes to manage salary
+ *
+ *  
+ * This function is used to manage salary
+ * 
+ *
+ */
+
 int emp_sal_mgmt();
+
+ /** @brief Function prototypes to add salary for new employee
+ *
+ *  
+ * This function is used to add salary for new employee
+ * 
+ *
+ */
+
 char* add_salary(int);
+
+#endif
+
+
+
