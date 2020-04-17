5a5c30395f8ac0176a7b0e46c97f539b7d896b2a
jagdeepbaidwan <baidwanjagdeep94@gmail.com>
2020-04-17 17:49:40 -0400

    header files commented

--- include/attendance_display.h
+++ include/attendance_display.h
@@ -1,1 +1,14 @@
+/** @file attendance_display.h
+ *  @brief Function prototypes to view attendance
+ *
+ *  
+ * This function is used to view attendance 
+ * 
+ *
+ */
+#ifndef attendance_display_H
+#define attendance_display_H
+
 void view_attendance(int);
+
+#endif
--- include/employee_personal_dtl_management.h
+++ include/employee_personal_dtl_management.h
@@ -1,6 +1,61 @@
+
+/** @file employee_personal_dtl_management.h
+ */
+#ifndef employee_personal_dtl_management_H
+#define employee_personal_dtl_management_H
+
+/**  @brief Function prototypes to view menu for employee personal details
+ *
+ *  
+ * This function is used to view menu for employee personal details
+ * 
+ *
+ */
 int emp_detail_mgmt(int );
+
+/**  @brief Function prototypes to raise grievances
+ *
+ *  
+ * This function is used to raise grievances
+ * 
+ *
+ */
 char* raise_grievances(int);
+
+/**  @brief Function prototypes to update details by employee
+ *
+ *  
+ * This function is used to update details by employee
+ * 
+ *
+ */
 char* update_employee(int );
+
+/**  @brief Function prototypes to view the raised grievances
+ *
+ *  
+ * This function is used to view the raised grievances
+ * 
+ *
+ */
 void view_raised_grievances();
+
+/**  @brief Function prototypes to give employee rating
+ *
+ *  
+ * This function is used to give employee rating
+ * 
+ *
+ */
 char* employee_rating();
+
+/**  @brief Function prototypes to display employee information
+ *
+ *  
+ * This function is used to display employee information
+ * 
+ *
+ */
 void emp_display(char []);
+
+#endif
